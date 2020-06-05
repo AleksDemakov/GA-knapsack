4614594f7befdfd5e909573c5f01826c5b1317c2
AleksDemakov <60163292+AleksDemakov@users.noreply.github.com>
2020-06-05 15:35:53 +0800

    init

---
+++ knapsack/solverga.cpp
@@ -0,0 +1,6 @@
+#include "solverga.h"
+
+SolverGA::SolverGA()
+{
+
+}
---
+++ knapsack/solverga.h
@@ -0,0 +1,12 @@
+#ifndef SOLVERGA_H
+#define SOLVERGA_H
+
+
+class SolverGA
+{
+public:
+    SolverGA();
+
+};
+
+#endif // SOLVERGA_H
