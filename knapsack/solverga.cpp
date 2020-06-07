27a86239bb44250b7e083967df27d30e55013342
AleksDemakov <60163292+AleksDemakov@users.noreply.github.com>
2020-06-07 15:36:11 +0800

    da
    crossover and sorting

--- knapsack/solverga.cpp
+++ knapsack/solverga.cpp
@@ -2,53 +2,70 @@
 #include <QVector>
 #include <QDebug>
 #include <QDateTime>
-class Individual
-{
+class SolverGA::Individual{
+
+    public:
     QVector<int> pocket;
     int n;
-    public:
-        Individual(int n)
+    SolverGA *solver;
+        Individual(SolverGA *solver)
         {
-            qsrand(QDateTime::currentMSecsSinceEpoch());
-            this->n = n;
+            this->solver = solver;
+            this->n = solver->numOfInd;
             pocket.resize(n);
             for(int i=0;i<n;i++){
                 pocket[i] = qrand() % 2;
             }
-            qDebug()<<pocket;
         }
-        int fitnessScore(QVector<int> &c)
+        int fitnessScore()
         {
             int sum_c = 0;
             for(int i=0;i<this->n;i++) {
                 if(pocket[i] == 1)
-                    sum_c += c[i];
+                    sum_c += solver->costs[i];
             }
             return sum_c;
         }
-        int weight(QVector<int> &w){
+        int weight(){
             int sum_w=0;
             for(int i=0;i<this->n;i++) {
                 if(pocket[i] == 1)
-                    sum_w += w[i];
+                    sum_w += solver->weights[i];
             }
             return sum_w;
         }
-
+        Individual *crossover(Individual *other){
+            Individual *son = new Individual(solver);
+            for(int i=0;i<this->n;i++){
+                if(this->pocket[i] == other->pocket[i])
+                    son->pocket[i] = this->pocket[i];
+                else
+                    son->pocket[i] = qrand()%2;
+            }
+            return son;
+        }
+        bool operator < (Individual *other){
+            return this->fitnessScore() < other->fitnessScore();
+        }
 };
 SolverGA::SolverGA(int n, int v, QVector<int> w, QVector<int> c)
 {
+    //
+    this->numOfInd = 5;
     this->n = n;
     this->limit = v;
     this->weights = w;
     this->costs = c;
-    Individual *test = new Individual(n);
+    population.resize(numOfInd);
+    //initiate population
 
-    QVector<Individual> population;
+    //*todo: better random
+    qsrand(QDateTime::currentMSecsSinceEpoch());
     for(int i=0;i<this->numOfInd;i++)
-        population.push_back(*test);
+        population[i] = new Individual(this);
+
 }
-
-Individual findFittest(){
-
+void SolverGA::sortPopulation(){
+    std::sort(population.begin(), population.end(),
+              [](Individual *a, Individual *b){return a->fitnessScore() < b->fitnessScore();});
 }
--- knapsack/solverga.h
+++ knapsack/solverga.h
@@ -6,16 +6,19 @@ class SolverGA
 {
 public:
     SolverGA(int n, int v, QVector<int> w, QVector<int> c);
-
+    void sortPopulation();
 
 protected:
+    class Individual;
     //problem data
     int n, limit;
     QVector<int> weights;
     QVector<int> costs;
 
+    QVector<Individual*> population;
+
     //cnt of individuals in population
-    int numOfInd = 5;
+    int numOfInd;
 
 };
 
